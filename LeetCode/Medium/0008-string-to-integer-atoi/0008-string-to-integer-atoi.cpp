af04b00e4ea501ea7f64a4dc5189654ed8685467
InderPreet Singh <inderpreet0071322@gmail.com>
2026-06-30 22:07:36 +0530

    0008-string-to-integer-atoi

---
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size(), sign = 1;
+        while (i < n && s[i] == ' ')
+            i++;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+        long long ans = 0;
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+            if (ans > (INT_MAX - digit) / 10)
+                return sign == 1 ? INT_MAX : INT_MIN;
+            ans = ans * 10 + digit;
+            i++;
+        }
+        return sign * ans;
+    }
+};
