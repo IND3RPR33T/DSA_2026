f1c3102498d555576c242a6728bc20bd8c3cc332
InderPreet Singh <inderpreet0071322@gmail.com>
2026-07-14 00:01:27 +0530

    0297-serialize-and-deserialize-binary-tree

---
+++ LeetCode/Hard/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,65 @@
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        if (!root) return "";
+
+        string s;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (!node) {
+                s += "#,";
+                continue;
+            }
+
+            s += to_string(node->val) + ",";
+            q.push(node->left);
+            q.push(node->right);
+        }
+
+        return s;
+    }
+
+    TreeNode* deserialize(string data) {
+        if (data.empty()) return nullptr;
+
+        stringstream ss(data);
+        string str;
+
+        getline(ss, str, ',');
+        TreeNode* root = new TreeNode(stoi(str));
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            getline(ss, str, ',');
+            if (str != "#") {
+                node->left = new TreeNode(stoi(str));
+                q.push(node->left);
+            }
+
+            getline(ss, str, ',');
+            if (str != "#") {
+                node->right = new TreeNode(stoi(str));
+                q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * Codec ser, deser;
+ * TreeNode* ans = deser.deserialize(ser.serialize(root));
+ */
