cb47aea3648e992b3033c29406a35cf950b6531e
InderPreet Singh <inderpreet0071322@gmail.com>
2026-08-09 12:17:39 +0530

    0217-contains-duplicate

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-      
-        for(int i = 1; i < nums.size();i++){
-            if(nums[i-1]==nums[i]) return true;
-        }
-        return false;
+         unordered_map<int , int> freq;
+        for(int num : nums)
+        {
+            freq[num]++;
+            if(freq[num] > 1)
+            {
+                return true;
+            }
+            
+        }return  false;
     }
 };
